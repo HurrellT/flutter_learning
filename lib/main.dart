afd627f3c010dd36dac75e23173fa6873e7c0f1e
Tomás Hurrell <hurrellt@gmail.com>
2019-06-21 14:48:30 -0300

    Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(home: new MyApp()));
+void main() => runApp(new MaterialApp(
+      home: new MyApp(),
+    ));
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() => new MyAppState();
 }
 
+enum MyDialogActions { yes, no, maybe }
+
 class MyAppState extends State<MyApp> {
-
-  //vars
   String _text = '';
-
-  final GlobalKey<ScaffoldState> _scaffoldState = new GlobalKey<ScaffoldState>();
-
-  //Methods
 
   void _onChange(String value) {
     setState(() {
@@ -24,32 +20,62 @@ class MyAppState extends State<MyApp> {
     });
   }
 
-  void _showSnackBar(String value) {
+  void _dialogResult(MyDialogActions value) {
+    print('You selected $value');
+    Navigator.pop(context);
+    //Switch on value
+  }
+
+  void _showAlert(String value) {
     if (value.isEmpty) return;
-    _scaffoldState.currentState.showSnackBar(
-      new SnackBar(
-        content: new Text(value),
-      )
+
+    AlertDialog dialog = new AlertDialog(
+      content: new Text(
+        value,
+        style: new TextStyle(fontSize: 30),
+      ),
+      actions: <Widget>[
+        new FlatButton(
+          onPressed: () {
+            _dialogResult(MyDialogActions.yes);
+          },
+          child: new Text('Yes'),
+        ),
+        new FlatButton(
+          onPressed: () {
+            _dialogResult(MyDialogActions.no);
+          },
+          child: new Text('No'),
+        ),
+        new FlatButton(
+          onPressed: () {
+            _dialogResult(MyDialogActions.maybe);
+          },
+          child: new Text('Maybe'),
+        ),
+      ],
     );
+
+    showDialog(context: context, builder: (BuildContext context) => dialog);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      key: _scaffoldState,
       appBar: new AppBar(
-        title: new Text('SnackBar Demo'),
+        title: new Text('Alert Dialog Demo'),
       ),
       body: new Container(
-        padding: new EdgeInsets.all(32),
+        padding: EdgeInsets.all(32),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new TextField(onChanged: _onChange,),
+              new TextField(onChanged: _onChange),
               new RaisedButton(
-                child: new Text('SnackBar!'),
-                onPressed: () {_showSnackBar(_text);},
-              )
+                  child: new Text('Alert Dialog!'),
+                  onPressed: () {
+                    _showAlert(_text);
+                  })
             ],
           ),
         ),
--- lib/oldMain.dart
+++ lib/oldMain.dart
@@ -237,6 +237,10 @@
 //
 //}
 
+
+//------------------------------------------------------------------------------------------------
+
+
 // NAVIGATORS AND ROUTERS
 
 //import 'package:flutter/cupertino.dart';
@@ -263,4 +267,70 @@
 //      );
 //  }
 //
+//}
+
+
+//------------------------------------------------------------------------------------------------
+
+
+//SNACKBAR
+//
+//import 'package:flutter/cupertino.dart';
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(new MaterialApp(home: new MyApp()));
+//
+//class MyApp extends StatefulWidget {
+//
+//  @override
+//  State<StatefulWidget> createState() => new MyAppState();
+//}
+//
+//class MyAppState extends State<MyApp> {
+//
+//  //vars
+//  String _text = '';
+//
+//  final GlobalKey<ScaffoldState> _scaffoldState = new GlobalKey<ScaffoldState>();
+//
+//  //Methods
+//
+//  void _onChange(String value) {
+//    setState(() {
+//      _text = value;
+//    });
+//  }
+//
+//  void _showSnackBar(String value) {
+//    if (value.isEmpty) return;
+//    _scaffoldState.currentState.showSnackBar(
+//        new SnackBar(
+//          content: new Text(value),
+//        )
+//    );
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      key: _scaffoldState,
+//      appBar: new AppBar(
+//        title: new Text('SnackBar Demo'),
+//      ),
+//      body: new Container(
+//        padding: new EdgeInsets.all(32),
+//        child: new Center(
+//          child: new Column(
+//            children: <Widget>[
+//              new TextField(onChanged: _onChange,),
+//              new RaisedButton(
+//                child: new Text('SnackBar!'),
+//                onPressed: () {_showSnackBar(_text);},
+//              )
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
 //}
