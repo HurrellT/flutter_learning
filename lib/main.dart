f39da92d21273e708110fe355828952f4b4e3694
Tomás Hurrell <hurrellt@gmail.com>
2019-06-21 15:16:18 -0300

    Checkboxes

--- lib/main.dart
+++ lib/main.dart
@@ -5,57 +5,47 @@ void main() => runApp(new MaterialApp(home: new MyApp(),));
 class MyApp extends StatefulWidget {
 
   @override
-  State<StatefulWidget> createState() => MyAppState();
+  State<StatefulWidget> createState() => new MyAppState();
 
 }
 
 class MyAppState extends State<MyApp> {
 
-  String _text = '';
+  bool _isChecked = false;
 
-  void _onPressed() {
-    print('The text is $_text');
-  }
-
-  void _onPressed2() {
-    print(_controller.text);
-  }
-
-  void _onChanged(String value) {
+  void onChanged(bool value) {
     setState(() {
-      _text = value;
+      _isChecked = value;
     });
   }
-
-  final TextEditingController _controller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Text Field demo'),
+        title: new Text('Checkbox Demo'),
       ),
       body: new Container(
         padding: new EdgeInsets.all(32),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new TextField(
-                onChanged: (String value){_onChanged(value);},
-                controller: _controller,
-                maxLines: 3,
-                autocorrect: true,
-                decoration: new InputDecoration(
-                  icon: new Icon(Icons.print),
-                  hintText: 'Type something here',
-                  labelText: 'What is your name'
-                ),
+              new Row(
+                children: <Widget>[
+                  new Text('Click it ->'),
+                  new Checkbox(
+                  value: _isChecked,
+                  onChanged: (bool value){onChanged(value);},
+                )
+                ],
               ),
-              new RaisedButton(
-                child: new Text('Click me!'),
-//                onPressed: (){_onPressed();}
-                onPressed: (){_onPressed2();}
-              ),
+              new CheckboxListTile(
+                title: new Text('Click me!'),
+                value: _isChecked,
+                activeColor: Colors.red,
+                secondary: const Icon(Icons.home),
+                onChanged: (bool value){onChanged(value);},
+              )
             ],
           ),
         ),
--- lib/oldMain.dart
+++ lib/oldMain.dart
@@ -425,3 +425,76 @@
 //    );
 //  }
 //}
+
+
+//------------------------------------------------------------------------------------------------
+
+//TEXT FIELD
+
+//
+//import 'package:flutter/material.dart';
+//
+//void main() => runApp(new MaterialApp(home: new MyApp(),));
+//
+//class MyApp extends StatefulWidget {
+//
+//  @override
+//  State<StatefulWidget> createState() => MyAppState();
+//
+//}
+//
+//class MyAppState extends State<MyApp> {
+//
+//  String _text = '';
+//
+//  void _onPressed() {
+//    print('The text is $_text');
+//  }
+//
+//  void _onPressed2() {
+//    print(_controller.text);
+//  }
+//
+//  void _onChanged(String value) {
+//    setState(() {
+//      _text = value;
+//    });
+//  }
+//
+//  final TextEditingController _controller = new TextEditingController();
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text('Text Field demo'),
+//      ),
+//      body: new Container(
+//        padding: new EdgeInsets.all(32),
+//        child: new Center(
+//          child: new Column(
+//            children: <Widget>[
+//              new TextField(
+//                onChanged: (String value){_onChanged(value);},
+//                controller: _controller,
+//                maxLines: 3,
+//                autocorrect: true,
+//                decoration: new InputDecoration(
+//                    icon: new Icon(Icons.print),
+//                    hintText: 'Type something here',
+//                    labelText: 'What is your name'
+//                ),
+//              ),
+//              new RaisedButton(
+//                  child: new Text('Click me!'),
+////                onPressed: (){_onPressed();}
+//                  onPressed: (){_onPressed2();}
+//              ),
+//            ],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//
+//}
