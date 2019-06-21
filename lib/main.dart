f859c91af35e59af5b89252e72c3c6cfae2a2da3
Tomás Hurrell <hurrellt@gmail.com>
2019-06-21 12:39:07 -0300

    AppBar + Login app

--- lib/main.dart
+++ lib/main.dart
@@ -1,140 +1,95 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'HelloWorldText.dart';
+import 'package:flutter_learning/Login.dart';
+import 'package:flutter_learning/Settings.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _MyAppState();
+
+}
+
+class _MyAppState extends State<MyApp> {
+
+  //Parameters
+
+  String _title = 'Please Login';
+  Widget _screen;
+  Login _login;
+  Settings _settings;
+  bool _isAuthenticated;
+
+
+  //Constructor
+
+  _MyAppState() {
+    _login = new Login(onSubmit: (){onSubmit();});
+    _settings = new Settings();
+    _screen = _login;
+    _isAuthenticated = false;
+  }
+
+
+  //Methods
+
+  void onSubmit() {
+    print('Login with: ' + _login.username + ' ' + _login.password);
+    if(_login.username == 'demo' && _login.password == 'demo') {
+      _setAuthenticated(true);
+    }
+  }
+
+  void _goHome() {
+    print('Go Home');
+
+    setState(() {
+      if(_isAuthenticated) {
+        _screen = _settings;
+      }
+      else {
+        _screen = _login;
+      }
+    });
+  }
+
+  void logout() {
+    print('Log out');
+    _setAuthenticated(false);
+  }
+
+  void _setAuthenticated(bool auth) {
+    setState(() {
+      if(auth) {
+        _screen = _settings;
+        _title = 'Welcome';
+        _isAuthenticated = true;
+      }
+      else {
+        _screen = _login;
+        _title = 'Please Login';
+        _isAuthenticated = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Basic Layouts',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-//      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      title: 'Login Demo',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Basic Layouts'),
+          title: new Text(_title),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.home), onPressed: (){_goHome();}),
+            new IconButton(icon: new Icon(Icons.exit_to_app), onPressed: (){logout();}),
+          ],
         ),
-        drawer: Drawer(
-          child: ListTile(
-            leading: Icon(Icons.change_history),
-            title: Text('Change history'),
-            onTap: () {
-              // change app state...
-              Navigator.pop(context); // close the drawer
-            },
-            subtitle: Text('Like a boss'),
-          ),
-        ),
-        body: new Container(
-          padding: const EdgeInsets.all(32.0),
-          child: new Center(
-            child: new Column(
-              children: <Widget>[
-                new HelloWorldText(),
-                new Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new HelloWorldText(),
-                    new HelloWorldText(),
-                    new HelloWorldText()
-                  ],
-                )
-              ],
-            ),
-          ),
-        ),
+        body: _screen,
       ),
     );
   }
-}
 
-//
-//class MyHomePage extends StatefulWidget {
-//  MyHomePage({Key key, this.title}) : super(key: key);
-//
-//  // This widget is the home page of your application. It is stateful, meaning
-//  // that it has a State object (defined below) that contains fields that affect
-//  // how it looks.
-//
-//  // This class is the configuration for the state. It holds the values (in this
-//  // case the title) provided by the parent (in this case the App widget) and
-//  // used by the build method of the State. Fields in a Widget subclass are
-//  // always marked "final".
-//
-//  final String title;
-//
-//  @override
-//  _MyHomePageState createState() => _MyHomePageState();
-//}
-//
-//class _MyHomePageState extends State<MyHomePage> {
-//  int _counter = 0;
-//
-//  void _incrementCounter() {
-//    setState(() {
-//      // This call to setState tells the Flutter framework that something has
-//      // changed in this State, which causes it to rerun the build method below
-//      // so that the display can reflect the updated values. If we changed
-//      // _counter without calling setState(), then the build method would not be
-//      // called again, and so nothing would appear to happen.
-//      _counter++;
-//    });
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    // This method is rerun every time setState is called, for instance as done
-//    // by the _incrementCounter method above.
-//    //
-//    // The Flutter framework has been optimized to make rerunning build methods
-//    // fast, so that you can just rebuild anything that needs updating rather
-//    // than having to individually change instances of widgets.
-//    return Scaffold(
-//      appBar: AppBar(
-//        // Here we take the value from the MyHomePage object that was created by
-//        // the App.build method, and use it to set our appbar title.
-//        title: Text(widget.title),
-//      ),
-//      body: Center(
-//        // Center is a layout widget. It takes a single child and positions it
-//        // in the middle of the parent.
-//        child: Column(
-//          // Column is also layout widget. It takes a list of children and
-//          // arranges them vertically. By default, it sizes itself to fit its
-//          // children horizontally, and tries to be as tall as its parent.
-//          //
-//          // Invoke "debug painting" (press "p" in the console, choose the
-//          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//          // to see the wireframe for each widget.
-//          //
-//          // Column has various properties to control how it sizes itself and
-//          // how it positions its children. Here we use mainAxisAlignment to
-//          // center the children vertically; the main axis here is the vertical
-//          // axis because Columns are vertical (the cross axis would be
-//          // horizontal).
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            Text(
-//              'You have pushed the button this many times:',
-//            ),
-//            Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.display1,
-//            ),
-//          ],
-//        ),
-//      ),
-//      floatingActionButton: FloatingActionButton(
-//        onPressed: _incrementCounter,
-//        tooltip: 'Increment',
-//        child: Icon(Icons.add),
-//      ), // This trailing comma makes auto-formatting nicer for build methods.
-//    );
-//  }
-//}
+}
