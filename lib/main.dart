5e3281b93985e6e43dd13075a3c6cd84a45d69e1
Tomás Hurrell <hurrellt@gmail.com>
2019-06-21 15:07:37 -0300

    Text Fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,66 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MaterialApp(
-      home: new MyApp(),
-    ));
+void main() => runApp(new MaterialApp(home: new MyApp(),));
 
 class MyApp extends StatefulWidget {
+
   @override
-  State<StatefulWidget> createState() => new MyAppState();
+  State<StatefulWidget> createState() => MyAppState();
+
 }
 
-enum MyDialogActions { yes, no, maybe }
-
 class MyAppState extends State<MyApp> {
+
   String _text = '';
 
-  void _onChange(String value) {
+  void _onPressed() {
+    print('The text is $_text');
+  }
+
+  void _onPressed2() {
+    print(_controller.text);
+  }
+
+  void _onChanged(String value) {
     setState(() {
       _text = value;
     });
   }
 
-  void _dialogResult(MyDialogActions value) {
-    print('You selected $value');
-    Navigator.pop(context);
-    //Switch on value
-  }
-
-  void _showAlert(String value) {
-    if (value.isEmpty) return;
-
-    AlertDialog dialog = new AlertDialog(
-      content: new Text(
-        value,
-        style: new TextStyle(fontSize: 30),
-      ),
-      actions: <Widget>[
-        new FlatButton(
-          onPressed: () {
-            _dialogResult(MyDialogActions.yes);
-          },
-          child: new Text('Yes'),
-        ),
-        new FlatButton(
-          onPressed: () {
-            _dialogResult(MyDialogActions.no);
-          },
-          child: new Text('No'),
-        ),
-        new FlatButton(
-          onPressed: () {
-            _dialogResult(MyDialogActions.maybe);
-          },
-          child: new Text('Maybe'),
-        ),
-      ],
-    );
-
-    showDialog(context: context, builder: (BuildContext context) => dialog);
-  }
+  final TextEditingController _controller = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Alert Dialog Demo'),
+        title: new Text('Text Field demo'),
       ),
       body: new Container(
-        padding: EdgeInsets.all(32),
+        padding: new EdgeInsets.all(32),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new TextField(onChanged: _onChange),
+              new TextField(
+                onChanged: (String value){_onChanged(value);},
+                controller: _controller,
+                maxLines: 3,
+                autocorrect: true,
+                decoration: new InputDecoration(
+                  icon: new Icon(Icons.print),
+                  hintText: 'Type something here',
+                  labelText: 'What is your name'
+                ),
+              ),
               new RaisedButton(
-                  child: new Text('Alert Dialog!'),
-                  onPressed: () {
-                    _showAlert(_text);
-                  })
+                child: new Text('Click me!'),
+//                onPressed: (){_onPressed();}
+                onPressed: (){_onPressed2();}
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
 }
---
+++ lib/mainPattern.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MaterialApp(home: new MyApp(),));
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+
+}
+
+class MyAppState extends State<MyApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+    );
+  }
+
+}
