4a1cf8c04c123d67a4b99a45d9b57c12181e192d
Tomás Hurrell <hurrellt@gmail.com>
2019-06-24 10:31:26 -0300

    Dropdowns

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(home: new MyApp(),));
@@ -11,10 +12,47 @@ class MyApp extends StatefulWidget {
 
 class MyAppState extends State<MyApp> {
 
+  String _value = '';
+//  List<String> values = new List<String>();
+  List<String> _values = new List();
+
+  @override
+  void initState() {
+    _values.addAll(['Bryan' , 'Chris', 'Heather' , 'Tommy']);
+    _value = _values.elementAt(0);
+  }
+
+  void _onChanged(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-
+      appBar: new AppBar(title: new Text('DropDownButton Demo'),),
+      body: new Container(
+        padding: new EdgeInsets.all(32),
+        child: new Column(
+          children: <Widget>[
+            new DropdownButton(
+              value: _value,
+              items: _values.map((String value){
+                return new DropdownMenuItem(
+                  value: value,
+                  child: new Row(
+                    children: <Widget>[
+                      new Icon(Icons.home),
+                      new Text('Person: ${value}')
+                    ],
+                  ),);
+              }).toList(),
+              onChanged: (String value) {_onChanged(value);},
+            )
+          ],
+        ),
+      ),
     );
   }
 
